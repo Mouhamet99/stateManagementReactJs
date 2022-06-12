5a4055ef0bd4034d9417edcc3c2b2f4ed65bc5b4
Mouhamet99 <dieyemouhamet1@gmail.com>
2022-06-12 06:51:05 +0000

    multi calls of setState method play each line of setState: this is a problem

--- src/Counter.js
+++ src/Counter.js
@@ -19,6 +19,8 @@ export default class Counter extends Component {
 
     increment() {
         this.setState(increment)
+        this.setState(increment)
+        this.setState(increment)
     }
     decrement() {
         this.setState((state) => {
