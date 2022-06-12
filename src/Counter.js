87603f7215fd4d6c623a1a6a1508bd19d0bb2e1c
Mouhamet99 <dieyemouhamet1@gmail.com>
2022-06-12 18:50:10 +0000

    know the difference between the old state and the new one using useRef hooks

--- src/Counter.js
+++ src/Counter.js
@@ -1,34 +1,41 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRe, useRef } from 'react'
 
-const getCountFromLocaleStorage = () => {
-    const countStorage = localStorage.getItem('count')
-    return countStorage ? JSON.parse(countStorage).count : { count: 0 }
-}
-const setCountToLocaleStorage = (count) => {
-    localStorage.setItem("count", JSON.stringify({ count }))
-}
+// const getCountFromLocaleStorage = () => {
+//     const countStorage = localStorage.getItem('count')
+//     return countStorage ? JSON.parse(countStorage).count : { count: 0 }
+// }
+// const setCountToLocaleStorage = (count) => {
+//     localStorage.setItem("count", JSON.stringify({ count }))
+// }
 
-const useLocaleStorage = (initialState, key) => {
-    const get = () => {
-        const storage = localStorage.getItem(key)
-        return storage ? JSON.parse(storage).value : initialState
-    }
+// const useLocaleStorage = (initialState, key) => {
+//     const get = () => {
+//         const storage = localStorage.getItem(key)
+//         return storage ? JSON.parse(storage).value : initialState
+//     }
 
-    const [value, setValue] = useState(get())
+//     const [value, setValue] = useState(get())
     
-    useEffect(() => {
-        localStorage.setItem(key, JSON.stringify({ value }))
-    }, [value])
+//     useEffect(() => {
+//         localStorage.setItem(key, JSON.stringify({ value }))
+//     }, [value])
 
-    return [value, setValue]
-}
+//     return [value, setValue]
+// }
 
 const Counter = ({ max, step }) => {
 
-    const [count, setCount] = useLocaleStorage(0, 'count')
+    const [count, setCount] = useState(0)
+    const countRef = useRef()
+    
+    if( countRef.current < count) console.log("Higher")
+    if( countRef.current > count) console.log("Lower")
+    countRef.current = count;
 
     useEffect(function () {
-        document.title = ` ${count}`
+        setTimeout(() =>{
+            console.log(`Counter  ${count}`)
+        }, 3000)
     }, [count]);
 
     const increment = () => {
